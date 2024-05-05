bf792fc4bd2e2b445934fe0dcae76667dd5a5e00
dieistowake <159567018+dieistowake@users.noreply.github.com>
2024-05-05 21:29:57 +0300

    asset implement/2.sınıf hayat bilgisi

--- Assets/2.SINIF/Hayat Bilgisi/Scripts/DropZone.cs
+++ Assets/2.SINIF/Hayat Bilgisi/Scripts/DropZone.cs
@@ -1,37 +1,68 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DropZone : MonoBehaviour, IDropHandler
 {
     [SerializeField] private Transform dropZoneTransform;
     public GameObject droppedObject;
     public HBDrag HBDragScript;
-    
+    public int maxLives = 3;
+    private int currentLives;
 
-    
+    public Text livesText;
+    public GameObject gameOverText;
+
+    private void Start()
+    {
+        currentLives = maxLives;
+        UpdateLivesText();
+        gameOverText.SetActive(false);
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log(gameObject.name);
         HBDragScript = eventData.pointerDrag.GetComponent<HBDrag>();
 
-        if(CheckDroppedObject(HBDragScript.gameObject.name, gameObject)){
+        if (CheckDroppedObject(HBDragScript.gameObject.name, gameObject))
+        {
             Debug.Log("OnDrop");
             HBDragScript.gameObject.SetActive(false);
         }
+        else
+        {
+            LoseLife();
+        }
 
+        if (currentLives <= 0)
+        {
+            EndGame();
+        }
     }
 
-    private void Awake()
+    public bool CheckDroppedObject(string droppedObject, GameObject dropZone)
     {
-        HBDragScript=droppedObject.GetComponent<HBDrag>();
-        Debug.Log("HBDragScript: " + HBDragScript);  
-    }
-    public bool CheckDroppedObject(string droppedObject, GameObject dropZone){
-        //dropZone.GetComponent<Collider2D>();
-        if(droppedObject==dropZone.name){
+        if (droppedObject == dropZone.name)
+        {
             return true;
         }
         return false;
+    }
+
+    private void LoseLife()
+    {
+        currentLives--;
+        UpdateLivesText();
+    }
+
+    private void UpdateLivesText()
+    {
+        livesText.text = "Can: " + currentLives.ToString();
+    }
+
+    private void EndGame()
+    {
+        gameOverText.SetActive(true);
     }
 }
--- Assets/2.SINIF/Hayat Bilgisi/Scripts/HBDrag.cs
+++ Assets/2.SINIF/Hayat Bilgisi/Scripts/HBDrag.cs
@@ -5,27 +5,30 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+
 public class HBDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
     private Vector2 initialPosition;
     public string clickedObjectName;
-
     public bool isInPlace;
 
+    private DropZone dropZone;
+    
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
-        canvasGroup=GetComponent<CanvasGroup>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        dropZone = FindObjectOfType<DropZone>(); // DropZone bileşenini bul
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         initialPosition = rectTransform.anchoredPosition;
-        canvasGroup.blocksRaycasts=false;
-        canvasGroup.alpha=.6f;
-        clickedObjectName=gameObject.name;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = .6f;
+        clickedObjectName = gameObject.name;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -35,8 +38,13 @@ public class HBDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvasGroup.blocksRaycasts=true;
-        canvasGroup.alpha=1f;
-    }
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
 
+        // Drag işlemi tamamlandığında, DropZone ile etkileşime geç
+        if (!isInPlace && dropZone != null)
+        {
+            dropZone.OnDrop(eventData);
+        }
+    }
 }
