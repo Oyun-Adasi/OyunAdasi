f3a39029d5dc84583d5eec605ebaf1aad95bdec8
dieistowake <159567018+dieistowake@users.noreply.github.com>
2024-05-12 22:39:29 +0300

    2.sınıf türkçe oyun
    soru/ses entegresi harici temeli hazırlandı

--- Assets/2.SINIF/Türkçe/Scripts/Vplay.cs
+++ Assets/2.SINIF/Türkçe/Scripts/Vplay.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 
 public class Vplay : MonoBehaviour
 {
-    [SerializeField] public AudioSource[] audioSources;
+    [SerializeField] public AudioClip[] audioClips;
     public string[] questions;
     public string[] answers;
     //public GameObject Restart;
@@ -24,137 +24,222 @@ public class Vplay : MonoBehaviour
     public Button buttonB;
     public Button buttonC;
     public TextMeshProUGUI q;
+    public Button voiceButton;
     bool[] takenAns;
     bool[] askedIndexes;
     int correctIndex;
 
-
     void Start()
     {
+        voiceButton.onClick.AddListener(PlayAudioAndShowQuestion);
         optionA = new CustomGameObject();
         optionA.gameObject = optiona;
         optionA.optionText = optiona.GetComponentInChildren<TextMeshProUGUI>();
-        optionA.button=buttonA;
+        optionA.button = buttonA;
         optionB = new CustomGameObject();
         optionB.gameObject = optionb;
         optionB.optionText = optionb.GetComponentInChildren<TextMeshProUGUI>();
-        optionB.button=buttonB;
+        optionB.button = buttonB;
         optionC = new CustomGameObject();
         optionC.gameObject = optionc;
         optionC.optionText = optionc.GetComponentInChildren<TextMeshProUGUI>();
-        optionC.button=buttonC;
-        optionArr=new CustomGameObject[3];
-        optionArr[0]=optionA;
-        optionArr[1]=optionB;
-        optionArr[2]=optionC;
-        takenAns=new bool[questions.Length];
-        askedIndexes=new bool[questions.Length];
+        optionC.button = buttonC;
+        optionArr = new CustomGameObject[3];
+        optionArr[0] = optionA;
+        optionArr[1] = optionB;
+        optionArr[2] = optionC;
+        takenAns = new bool[questions.Length];
+        askedIndexes = new bool[questions.Length];
         playGame();
     }
 
-    
     void Update()
     {
 
     }
-    void OptionModifier(int correctIndex, string[] arr){
-    int a = Random.Range(0, optionArr.Length);
-    switch (a)
-    {
-        case 0:
-            optionA.optionText.text = arr[correctIndex];
-            optionA.taken = true;
-            break;
-        case 1:
-            optionB.optionText.text = arr[correctIndex];
-            optionB.taken = true;
-            break;
-        case 2:
-            optionC.optionText.text = arr[correctIndex];
-            optionC.taken = true;
-            break;
-    }
 
-    List<string> unusedAns = new List<string>(arr);
-    unusedAns.RemoveAt(correctIndex);
-
-    for (int i = 0; i < optionArr.Length; i++)
+    void OptionModifier(int correctIndex, string[] arr)
     {
-        if (!optionArr[i].taken)
+        int a = Random.Range(0, optionArr.Length);
+        switch (a)
         {
-            int randomIndex = Random.Range(0, unusedAns.Count);
-            optionArr[i].optionText.text = unusedAns[randomIndex];
-            unusedAns.RemoveAt(randomIndex);
-            optionArr[i].taken = true;
+            case 0:
+                optionA.optionText.text = arr[correctIndex];
+                optionA.taken = true;
+                break;
+            case 1:
+                optionB.optionText.text = arr[correctIndex];
+                optionB.taken = true;
+                break;
+            case 2:
+                optionC.optionText.text = arr[correctIndex];
+                optionC.taken = true;
+                break;
         }
-    }
-    }
-    void resetOptions(){
-        optionA.taken=false;
-        optionB.taken=false;
-        optionC.taken=false;
-        
-    }
-    int CorrectAnswerDecider(bool[] arr){
-        int numAsked=0;
-        int index;
-        for(int i=0;i<arr.Length;i++){
-            if(arr[i])
-                numAsked++;
-        }
-        if(numAsked==arr.Length){
-            options(1);
-            //Restart.SetActive(true);
-            return -1;
-        }
-        else{
-            index=Random.Range(0,arr.Length);
-            while(arr[index]){
-                index=Random.Range(0,arr.Length);
+
+        List<string> unusedAns = new List<string>(arr);
+        unusedAns.RemoveAt(correctIndex);
+
+        for (int i = 0; i < optionArr.Length; i++)
+        {
+            if (!optionArr[i].taken)
+            {
+                int randomIndex = Random.Range(0, unusedAns.Count);
+                optionArr[i].optionText.text = unusedAns[randomIndex];
+                unusedAns.RemoveAt(randomIndex);
+                optionArr[i].taken = true;
             }
-        return index;
         }
     }
+
+    void PlayAudioAndShowQuestion()
+    {
+        int randomIndex = Random.Range(0, audioClips.Length);
+        AudioSource.PlayClipAtPoint(audioClips[randomIndex], Vector3.zero);
+
+        ShowQuestionAndAnswers(randomIndex);
+    }
+
+    void ShowQuestionAndAnswers(int audioIndex)
+    {
+        // Soruyu göster
+        int questionIndex = CorrectAnswerDecider(askedIndexes);
+        if (questionIndex == -1)
+        {
+            // Game Over handling
+            return;
+        }
+
+        while (askedIndexes[questionIndex])
+        {
+            questionIndex = Random.Range(0, questions.Length);
+        }
+
+        // Audio dosyasını çal
+        AudioSource.PlayClipAtPoint(audioClips[audioIndex], Vector3.zero);
+
+        q.text = questions[questionIndex];
+        takenAns[questionIndex] = true;
+        OptionModifier(questionIndex, questions);
+        askedIndexes[questionIndex] = true;
+    }
+
+    void CheckAnswer(int correctIndex)
+    {
+        if (optionA.button.interactable || optionB.button.interactable || optionC.button.interactable)
+        {
+            return;
+        }
+
+        if (correctIndex == 0 && optionA.button.interactable)
+        {
+            // Correct answer
+            // Handle correct answer
+            playGame();
+        }
+        else if (correctIndex == 1 && optionB.button.interactable)
+        {
+            // Correct answer
+            // Handle correct answer
+            playGame();
+        }
+        else if (correctIndex == 2 && optionC.button.interactable)
+        {
+            // Correct answer
+            // Handle correct answer
+            playGame();
+        }
+        else
+        {
+            // Incorrect answer
+            // Show same question again
+            ShowQuestionAndAnswers(correctIndex);
+        }
+    }
+
+    public void OptionSelected(int optionIndex)
+    {
+        CheckAnswer(correctIndex);
+    }
+
     public void Reset()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    void options(int a){
-        if(a==0){
-        optionA.gameObject.SetActive(true);
-        optionB.gameObject.SetActive(true);
-        optionC.gameObject.SetActive(true);
+
+    void options(int a)
+    {
+        if (a == 0)
+        {
+            optionA.gameObject.SetActive(true);
+            optionB.gameObject.SetActive(true);
+            optionC.gameObject.SetActive(true);
         }
-        else{
-        optionA.gameObject.SetActive(false);
-        optionB.gameObject.SetActive(false);
-        optionC.gameObject.SetActive(false);
-        }
-    }
-    void ResetAskedArr(){
-        for(int i=0;i<3;i++){
-            askedIndexes[i]=false;
-            
+        else
+        {
+            optionA.gameObject.SetActive(false);
+            optionB.gameObject.SetActive(false);
+            optionC.gameObject.SetActive(false);
         }
     }
-    public void playGame(){
+
+    void ResetAskedArr()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            askedIndexes[i] = false;
+        }
+    }
+
+    public void playGame()
+    {
         options(0);
-        correctIndex=CorrectAnswerDecider(askedIndexes);
-        if(correctIndex==-1){
+        correctIndex = CorrectAnswerDecider(askedIndexes);
+        if (correctIndex == -1)
+        {
             //GameOver();
         }
-        else{
-            while(askedIndexes[correctIndex]){
-                correctIndex=Random.Range(0,questions.Length);
+        else
+        {
+            while (askedIndexes[correctIndex])
+            {
+                correctIndex = Random.Range(0, questions.Length);
             }
-            q.text=questions[correctIndex];
-            takenAns[correctIndex]=true;
+            q.text = questions[correctIndex];
+            takenAns[correctIndex] = true;
             OptionModifier(correctIndex, questions);
-            askedIndexes[correctIndex]=true;
+            askedIndexes[correctIndex] = true;
         }
     }
-    public void audioPlay(int correctIndex){
-        //audioSources[correctIndex].Play();
+
+    public void audioPlay(int correctIndex)
+    {
         Debug.Log(correctIndex);
+    }
+
+    int CorrectAnswerDecider(bool[] arr)
+    {
+        int numAsked = 0;
+        int index;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i])
+                numAsked++;
+        }
+        if (numAsked == arr.Length)
+        {
+            options(1);
+            //Restart.SetActive(true);
+            return -1;
+        }
+        else
+        {
+            index = Random.Range(0, arr.Length);
+            while (arr[index])
+            {
+                index = Random.Range(0, arr.Length);
+            }
+            return index;
+        }
     }
 }
