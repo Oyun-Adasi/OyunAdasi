c99a1755abac5b1dc8245e76f2a791cc97b0c56d
timuroncu <timuroncu@yahoo.com>
2024-05-12 16:36:11 +0300

    Bugfixing
    As per title

--- Assets/3.SINIF/HAYAT BILGISI/Scripts/Game.cs
+++ Assets/3.SINIF/HAYAT BILGISI/Scripts/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,17 @@ using Random = UnityEngine.Random;
 using UnityEngine.EventSystems;
 public class Game : MonoBehaviour
 {
+    public Text anlikHizText;
     private string answertag;
-    public Button[] buttonlar;
-    public SpriteRenderer levhapanel;
+    public Image levhapanel;
     [SerializeField] private List<Sprite> levhalar;
     private string dogruButton;
     private int score;
+    public int anlikHiz;
     void Start()
     {
-        int randomIndex = Random.Range(0, 3);
+        anlikHiz = int.Parse(anlikHizText.text);
+        int randomIndex = Random.Range(0, 6);
         levhapanel.sprite = levhalar[randomIndex];
 
         if (randomIndex == 0)
@@ -25,9 +28,48 @@ public class Game : MonoBehaviour
         {
             dogruButton = "sol";
         }
-        else
+        else if (randomIndex == 2)
         {
             dogruButton = "sag";
+        }
+        else if (randomIndex == 3)
+        {
+            if (anlikHiz <= 30)
+            {
+                dogruButton = "gaz";
+                anlikHiz = 30;
+            }
+            else
+            {
+                dogruButton = "fren";
+                anlikHiz = 30;
+            }
+        }
+        else if (randomIndex == 4)
+        {
+            if (anlikHiz <= 40)
+            {
+                dogruButton = "gaz";
+                anlikHiz = 40;
+            }
+            else
+            {
+                dogruButton = "fren";
+                anlikHiz = 40;
+            }
+        }
+        else if (randomIndex == 5)
+        {
+            if (anlikHiz <= 50)
+            {
+                dogruButton = "gaz";
+                anlikHiz = 50;
+            }
+            else
+            {
+                dogruButton = "fren";
+                anlikHiz = 50;
+            }
         }
     }
     
@@ -41,6 +83,7 @@ public class Game : MonoBehaviour
         answertag = EventSystem.current.currentSelectedGameObject.tag;
         if (dogruButton == answertag)
         {
+            anlikHizText.text = anlikHiz.ToString();
             score++;
             Debug.Log("Tebrikler! Doğru cevap.");
             Start();
